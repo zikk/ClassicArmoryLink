b981ff5f4a1054f241d914cbf129791f139acf87
Zakaria Abdelaziz <zikk.abdelaziz@gmail.com>
2023-11-13 21:01:03 +0100

    feat(v1.1): add realms slug search by name and hide armory button for tabs other than character

--- ClassicArmoryLink.lua
+++ ClassicArmoryLink.lua
@@ -1,34 +1,40 @@
+local regionName = GetCurrentRegionName():lower()
+local realms
+
+if regionName == "eu" then
+    realms = EuRealmsList
+elseif regionName == "us" then
+    realms = UsRealmsList
+end
+
 -- Global variable to keep track of the current armory frame
 local currentArmoryFrame = nil
 
-local function CreateArmoryFrame(characterFullName, regionName)
+function GetRealmSlugByName(realmName)
+    for _, realm in ipairs(realms) do
+        for _, name in pairs(realm.name) do
+            if name == realmName then
+                return realm.slug
+            end
+        end
+    end
+    -- return nil -- Return nil if no match is found
+end
+
+local function CreateArmoryFrame(characterName, realmName)
      -- Close the existing frame if it's open
     if currentArmoryFrame and currentArmoryFrame:IsShown() then
         currentArmoryFrame:Hide()
     end
 
-    -- Function to convert realm name to a slug
-    local function RealmToSlug(realmName)
-        return realmName:gsub("%s+", ""):lower() -- Replace spaces with nothing and lowercase
-    end
-
-    local characterName, realmName = "unknown", "unknown"
-
-    if characterFullName then
-        characterName, realmName = string.match(characterFullName, "^(.+)-(.+)$")
-        if not realmName then -- If the character is from the same realm, realmName will be nil
-            characterName, realmName = characterFullName, GetRealmName()
-        end
-    end
-
-    local realmSlug = RealmToSlug(realmName)
+    local realmSlug = GetRealmSlugByName(realmName)
 
     -- Generate the URL
     local armoryURL = string.format("https://classic-armory.info/%s/classic/%s/%s", regionName, realmSlug, characterName:lower())
 
     local armoryFrame = CreateFrame("Frame", "ArmoryFrame", UIParent, "BackdropTemplate")
     armoryFrame:SetPoint("CENTER")
-    armoryFrame:SetSize(300, 100)
+    armoryFrame:SetSize(600, 70)
     armoryFrame:SetBackdrop({
         bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
         edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
@@ -43,7 +49,7 @@ local function CreateArmoryFrame(characterFullName, regionName)
 
     -- Create the EditBox within the frame
     local editBox = CreateFrame("EditBox", "ArmoryLinkBox", armoryFrame, "InputBoxTemplate")
-    editBox:SetSize(260, 20)
+    editBox:SetSize(550, 20)
     editBox:SetPoint("CENTER", armoryFrame, "CENTER", 0, 0)
     local armoryURL = armoryURL -- Replace with your desired URL
     editBox:SetText(armoryURL)
@@ -64,14 +70,31 @@ local function CreateArmoryButton(parentFrame, isInspect)
     if not parentFrame then return end -- Guard clause to ensure parentFrame is not nil
 
     local armoryButton = CreateFrame("Button", nil, parentFrame, "UIPanelButtonTemplate")
-    armoryButton:SetSize(80, 22)
+    armoryButton:SetSize(70, 20)
     armoryButton:SetText("Armory")
     armoryButton:SetFrameStrata("HIGH")
 
     if isInspect then
         armoryButton:SetPoint("TOPLEFT", parentFrame, "TOPLEFT", 160, -50)
     else
-        armoryButton:SetPoint("TOPRIGHT", parentFrame, "TOPRIGHT", -90, -80)
+        armoryButton:SetPoint("TOPRIGHT", parentFrame, "TOPLEFT", 135, -76)
+        armoryButton:Hide() -- Start hidden for Character Frame
+    end
+
+    local function UpdateButtonVisibility()
+        if not isInspect then
+            local selectedTab = PanelTemplates_GetSelectedTab(CharacterFrame)
+            if selectedTab == 1 then
+                armoryButton:Show()
+            else
+                armoryButton:Hide()
+            end
+        end
+    end
+
+    if not isInspect then
+        hooksecurefunc("CharacterFrameTab_OnClick", UpdateButtonVisibility)
+        CharacterFrame:HookScript("OnShow", UpdateButtonVisibility)
     end
 
     -- Button visibility tied to the parent frame's visibility
@@ -80,15 +103,14 @@ local function CreateArmoryButton(parentFrame, isInspect)
     parentFrame:HookScript("OnHide", function() armoryButton:Hide() end)
 
     armoryButton:SetScript("OnClick", function()
-        local characterFullName, realmName
-        local regionName = GetCurrentRegionName():lower()
+        local characterName, realmName
 
         if isInspect then
-            characterFullName = UnitName("target")
+            characterName, realmName = UnitName("target", true)
         else
-            characterFullName = UnitName("player") .. "-" .. GetRealmName()
+            characterName = UnitName("player", true)
         end
-        local armoryFrame = CreateArmoryFrame(characterFullName, regionName)
+        local armoryFrame = CreateArmoryFrame(characterName, realmName or GetRealmName())
         armoryFrame:Show()
     end)
 end
@@ -97,7 +119,7 @@ local eventFrame = CreateFrame("Frame")
 eventFrame:RegisterEvent("ADDON_LOADED")
 eventFrame:RegisterEvent("INSPECT_READY")
 eventFrame:SetScript("OnEvent", function(self, event, arg1)
-    if event == "ADDON_LOADED" and arg1 == "ArmoryLink" then
+    if event == "ADDON_LOADED" and arg1 == "ClassicArmoryLink" then
         CreateArmoryButton(CharacterFrame, false)
     elseif event == "INSPECT_READY" then
         CreateArmoryButton(InspectFrame, true)
---
+++ UsRealmsList.lua
@@ -0,0 +1,416 @@
+UsRealmsList = {
+  {
+    id = 4795,
+    slug = 'angerforge',
+    name = {
+      it_IT = 'Angerforge',
+      ru_RU = 'Angerforge',
+      en_GB = 'Angerforge',
+      zh_TW = '安格弗',
+      ko_KR = 'Angerforge',
+      en_US = 'Angerforge',
+      es_MX = 'Angerforge',
+      pt_BR = 'Angerforge',
+      es_ES = 'Angerforge',
+      zh_CN = '怒炉',
+      fr_FR = 'Angerforge',
+      de_DE = 'Angerforge'
+    }
+  },
+  {
+    id = 4384,
+    slug = 'mankrik',
+    name = {
+      it_IT = 'Mankrik',
+      ru_RU = 'Mankrik',
+      en_GB = 'Mankrik',
+      zh_TW = '曼寇里克',
+      ko_KR = 'Mankrik',
+      en_US = 'Mankrik',
+      es_MX = 'Mankrik',
+      pt_BR = 'Mankrik',
+      es_ES = 'Mankrik',
+      zh_CN = '曼科里克',
+      fr_FR = 'Mankrik',
+      de_DE = 'Mankrik'
+    }
+  },
+  {
+    id = 4738,
+    slug = 'maladath',
+    name = {
+      it_IT = 'Maladath',
+      ru_RU = 'Nightslayer',
+      en_GB = 'Maladath',
+      zh_TW = '瑪拉達斯',
+      ko_KR = 'Maladath',
+      en_US = 'Maladath',
+      es_MX = 'Maladath',
+      pt_BR = 'Maladath',
+      es_ES = 'Maladath',
+      zh_CN = '玛拉达斯',
+      fr_FR = 'Maladath',
+      de_DE = 'Maladath'
+    }
+  },
+  {
+    id = 4800,
+    slug = 'eranikus',
+    name = {
+      it_IT = 'Eranikus',
+      ru_RU = 'Eranikus',
+      en_GB = 'Eranikus',
+      zh_TW = '伊蘭尼庫斯',
+      ko_KR = 'Eranikus',
+      en_US = 'Eranikus',
+      es_MX = 'Eranikus',
+      pt_BR = 'Eranikus',
+      es_ES = 'Eranikus',
+      zh_CN = '伊兰尼库斯',
+      fr_FR = 'Eranikus',
+      de_DE = 'Eranikus'
+    }
+  },
+  {
+    id = 4385,
+    slug = 'pagle',
+    name = {
+      it_IT = 'Pagle',
+      ru_RU = 'Pagle',
+      en_GB = 'Pagle',
+      zh_TW = '帕格',
+      ko_KR = 'Pagle',
+      en_US = 'Pagle',
+      es_MX = 'Pagle',
+      pt_BR = 'Pagle',
+      es_ES = 'Pagle',
+      zh_CN = '帕格',
+      fr_FR = 'Pagle',
+      de_DE = 'Pagle'
+    }
+  },
+  {
+    id = 4408,
+    slug = 'faerlina',
+    name = {
+      it_IT = 'Faerlina',
+      ru_RU = 'Faerlina',
+      en_GB = 'Faerlina',
+      zh_TW = '費琳娜',
+      ko_KR = 'Faerlina',
+      en_US = 'Faerlina',
+      es_MX = 'Faerlina',
+      pt_BR = 'Faerlina',
+      es_ES = 'Faerlina',
+      zh_CN = '法琳娜',
+      fr_FR = 'Faerlina',
+      de_DE = 'Faerlina'
+    }
+  },
+  {
+    id = 4387,
+    slug = 'ashkandi',
+    name = {
+      it_IT = 'Ashkandi',
+      ru_RU = 'Ashkandi',
+      en_GB = 'Ashkandi',
+      zh_TW = '阿什坎迪',
+      ko_KR = 'Ashkandi',
+      en_US = 'Ashkandi',
+      es_MX = 'Ashkandi',
+      pt_BR = 'Ashkandi',
+      es_ES = 'Ashkandi',
+      zh_CN = '阿什坎迪',
+      fr_FR = 'Ashkandi',
+      de_DE = 'Ashkandi'
+    }
+  },
+  {
+    id = 4727,
+    slug = 'windseeker',
+    name = {
+      it_IT = 'Windseeker',
+      ru_RU = 'Windseeker',
+      en_GB = 'Windseeker',
+      zh_TW = '逐風者',
+      ko_KR = 'Windseeker',
+      en_US = 'Windseeker',
+      es_MX = 'Windseeker',
+      pt_BR = 'Windseeker',
+      es_ES = 'Windseeker',
+      zh_CN = '逐风者',
+      fr_FR = 'Windseeker',
+      de_DE = 'Windseeker'
+    }
+  },
+  {
+    id = 4731,
+    slug = 'earthfury',
+    name = {
+      it_IT = 'Earthfury',
+      ru_RU = 'Earthfury',
+      en_GB = 'Earthfury',
+      zh_TW = '大地之怒',
+      ko_KR = 'Earthfury',
+      en_US = 'Earthfury',
+      es_MX = 'Earthfury',
+      pt_BR = 'Earthfury',
+      es_ES = 'Earthfury',
+      zh_CN = '大地之怒',
+      fr_FR = 'Earthfury',
+      de_DE = 'Earthfury'
+    }
+  },
+  {
+    id = 4667,
+    slug = 'remulos',
+    name = {
+      it_IT = 'Remulos',
+      ru_RU = 'Remulos',
+      en_GB = 'Remulos',
+      zh_TW = '雷姆洛斯',
+      ko_KR = 'Remulos',
+      en_US = 'Remulos',
+      es_MX = 'Remulos',
+      pt_BR = 'Remulos',
+      es_ES = 'Remulos',
+      zh_CN = '雷姆洛斯',
+      fr_FR = 'Remulos',
+      de_DE = 'Remulos'
+    }
+  },
+  {
+    id = 4669,
+    slug = 'arugal',
+    name = {
+      it_IT = 'Arugal',
+      ru_RU = 'Arugal',
+      en_GB = 'Arugal',
+      zh_TW = '阿魯高',
+      ko_KR = 'Arugal',
+      en_US = 'Arugal',
+      es_MX = 'Arugal',
+      pt_BR = 'Arugal',
+      es_ES = 'Arugal',
+      zh_CN = '阿鲁高',
+      fr_FR = 'Arugal',
+      de_DE = 'Arugal'
+    }
+  },
+  {
+    id = 4728,
+    slug = 'benediction',
+    name = {
+      it_IT = 'Benediction',
+      ru_RU = 'Benediction',
+      en_GB = 'Benediction',
+      zh_TW = '祈福',
+      ko_KR = 'Benediction',
+      en_US = 'Benediction',
+      es_MX = 'Benediction',
+      pt_BR = 'Benediction',
+      es_ES = 'Benediction',
+      zh_CN = '祈福',
+      fr_FR = 'Benediction',
+      de_DE = 'Benediction'
+    }
+  },
+  {
+    id = 4670,
+    slug = 'yojamba',
+    name = {
+      it_IT = 'Yojamba',
+      ru_RU = 'Yojamba',
+      en_GB = 'Yojamba',
+      zh_TW = '尤亞姆巴',
+      ko_KR = 'Yojamba',
+      en_US = 'Yojamba',
+      es_MX = 'Yojamba',
+      pt_BR = 'Yojamba',
+      es_ES = 'Yojamba',
+      zh_CN = '尤亚姆巴',
+      fr_FR = 'Yojamba',
+      de_DE = 'Yojamba'
+    }
+  },
+  {
+    id = 4388,
+    slug = 'westfall',
+    name = {
+      it_IT = 'Westfall',
+      ru_RU = 'Westfall',
+      en_GB = 'Westfall',
+      zh_TW = '西部荒野',
+      ko_KR = 'Westfall',
+      en_US = 'Westfall',
+      es_MX = 'Westfall',
+      pt_BR = 'Westfall',
+      es_ES = 'Westfall',
+      zh_CN = '西部荒野',
+      fr_FR = 'Westfall',
+      de_DE = 'Westfall'
+    }
+  },
+  {
+    id = 4726,
+    slug = 'sulfuras',
+    name = {
+      it_IT = 'Sulfuras',
+      ru_RU = 'Heartseeker',
+      en_GB = 'Sulfuras',
+      zh_TW = '薩弗拉斯',
+      ko_KR = 'Sulfuras',
+      en_US = 'Sulfuras',
+      es_MX = 'Sulfuras',
+      pt_BR = 'Sulfuras',
+      es_ES = 'Sulfuras',
+      zh_CN = '萨弗拉斯',
+      fr_FR = 'Sulfuras',
+      de_DE = 'Sulfuras'
+    }
+  },
+  {
+    id = 4376,
+    slug = 'azuresong',
+    name = {
+      it_IT = 'Azuresong',
+      ru_RU = 'Azuresong',
+      en_GB = 'Azuresong',
+      zh_TW = '碧空之歌',
+      ko_KR = 'Azuresong',
+      en_US = 'Azuresong',
+      es_MX = 'Azuresong',
+      pt_BR = 'Azuresong',
+      es_ES = 'Azuresong',
+      zh_CN = '碧空之歌',
+      fr_FR = 'Azuresong',
+      de_DE = 'Azuresong'
+    }
+  },
+  {
+    id = 4373,
+    slug = 'myzrael',
+    name = {
+      it_IT = 'Myzrael',
+      ru_RU = 'Myzrael',
+      en_GB = 'Myzrael',
+      zh_TW = '密斯賴爾',
+      ko_KR = 'Myzrael',
+      en_US = 'Myzrael',
+      es_MX = 'Myzrael',
+      pt_BR = 'Myzrael',
+      es_ES = 'Myzrael',
+      zh_CN = '密斯莱尔',
+      fr_FR = 'Myzrael',
+      de_DE = 'Myzrael'
+    }
+  },
+  {
+    id = 4395,
+    slug = 'whitemane',
+    name = {
+      it_IT = 'Whitemane',
+      ru_RU = 'Whitemane',
+      en_GB = 'Whitemane',
+      zh_TW = '懷特邁恩',
+      ko_KR = 'Whitemane',
+      en_US = 'Whitemane',
+      es_MX = 'Whitemane',
+      pt_BR = 'Whitemane',
+      es_ES = 'Whitemane',
+      zh_CN = '怀特迈恩',
+      fr_FR = 'Whitemane',
+      de_DE = 'Whitemane'
+    }
+  },
+  {
+    id = 4725,
+    slug = 'skyfury',
+    name = {
+      it_IT = 'Skyfury',
+      ru_RU = 'Anathema',
+      en_GB = 'Skyfury',
+      zh_TW = '天怒',
+      ko_KR = 'Skyfury',
+      en_US = 'Skyfury',
+      es_MX = 'Skyfury',
+      pt_BR = 'Skyfury',
+      es_ES = 'Skyfury',
+      zh_CN = '天怒',
+      fr_FR = 'Skyfury',
+      de_DE = 'Skyfury'
+    }
+  },
+  {
+    id = 4374,
+    slug = 'old-blanchy',
+    name = {
+      it_IT = 'Old Blanchy',
+      ru_RU = 'Old Blanchy',
+      en_GB = 'Old Blanchy',
+      zh_TW = '老馬布蘭契',
+      ko_KR = 'Old Blanchy',
+      en_US = 'Old Blanchy',
+      es_MX = 'Old Blanchy',
+      pt_BR = 'Old Blanchy',
+      es_ES = 'Old Blanchy',
+      zh_CN = '老马布兰契',
+      fr_FR = 'Old Blanchy',
+      de_DE = 'Old Blanchy'
+    }
+  },
+  {
+    id = 4372,
+    slug = 'atiesh',
+    name = {
+      it_IT = 'Atiesh',
+      ru_RU = 'Atiesh',
+      en_GB = 'Atiesh',
+      zh_TW = '阿泰絲',
+      ko_KR = 'Atiesh',
+      en_US = 'Atiesh',
+      es_MX = 'Atiesh',
+      pt_BR = 'Atiesh',
+      es_ES = 'Atiesh',
+      zh_CN = '埃提耶什',
+      fr_FR = 'Atiesh',
+      de_DE = 'Atiesh'
+    }
+  },
+  {
+    id = 4647,
+    slug = 'grobbulus',
+    name = {
+      it_IT = 'Grobbulus',
+      ru_RU = 'Grobbulus',
+      en_GB = 'Grobbulus',
+      zh_TW = '葛羅巴斯',
+      ko_KR = 'Grobbulus',
+      en_US = 'Grobbulus',
+      es_MX = 'Grobbulus',
+      pt_BR = 'Grobbulus',
+      es_ES = 'Grobbulus',
+      zh_CN = '格罗布鲁斯',
+      fr_FR = 'Grobbulus',
+      de_DE = 'Grobbulus'
+    }
+  },
+  {
+    id = 4648,
+    slug = 'bloodsail-buccaneers',
+    name = {
+      it_IT = 'Bloodsail Buccaneers',
+      ru_RU = 'Bloodsail Buccaneers',
+      en_GB = 'Bloodsail Buccaneers',
+      zh_TW = '血帆海盜',
+      ko_KR = 'Bloodsail Buccaneers',
+      en_US = 'Bloodsail Buccaneers',
+      es_MX = 'Bloodsail Buccaneers',
+      pt_BR = 'Bloodsail Buccaneers',
+      es_ES = 'Bloodsail Buccaneers',
+      zh_CN = '血帆海盗',
+      fr_FR = 'Bloodsail Buccaneers',
+      de_DE = 'Bloodsail Buccaneers'
+    }
+  }
+}
