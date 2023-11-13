e015145da269909575be2c0dd920048365f525f8
Zakaria Abdelaziz <zikk.abdelaziz@gmail.com>
2023-11-13 20:48:37 +0100

    feat(v1.1): add realms slug search by name

---
+++ EuRealmsList.lua
@@ -0,0 +1,455 @@
+local euRealms = {
+  {
+    id = 4749,
+    slug = "earthshaker",
+    name = {
+      it_IT = "Earthshaker",
+      ru_RU = "Землекрушитель",
+      en_GB = "Earthshaker",
+      zh_TW = "震地者",
+      ko_KR = "Earthshaker",
+      en_US = "Earthshaker",
+      es_MX = "Earthshaker",
+      pt_BR = "Earthshaker",
+      es_ES = "Earthshaker",
+      zh_CN = "震地者",
+      fr_FR = "Earthshaker",
+      de_DE = "Earthshaker"
+    }
+  },
+  {
+    id = 4455,
+    slug = "razorfen",
+    name = {
+      it_IT = "Razorfen",
+      ru_RU = "Razorfen",
+      en_GB = "Razorfen",
+      zh_TW = "剃刀高地",
+      ko_KR = "Razorfen",
+      en_US = "Razorfen",
+      es_MX = "Razorfen",
+      pt_BR = "Razorfen",
+      es_ES = "Razorfen",
+      zh_CN = "剃刀",
+      fr_FR = "Razorfen",
+      de_DE = "Razorfen"
+    }
+  },
+  {
+    id = 4678,
+    slug = "hydraxian-waterlords",
+    name = {
+      it_IT = "Hydraxian Waterlords",
+      ru_RU = "Hydraxian Waterlords",
+      en_GB = "Hydraxian Waterlords",
+      zh_TW = "海達希亞水元素",
+      ko_KR = "Hydraxian Waterlords",
+      en_US = "Hydraxian Waterlords",
+      es_MX = "Hydraxian Waterlords",
+      pt_BR = "Hydraxian Waterlords",
+      es_ES = "Hydraxian Waterlords",
+      zh_CN = "海达希亚水元素",
+      fr_FR = "Hydraxian Waterlords",
+      de_DE = "Hydraxian Waterlords"
+    }
+  },
+  {
+    id = 4477,
+    slug = "venoxis",
+    name = {
+      it_IT = "Venoxis",
+      ru_RU = "Venoxis",
+      en_GB = "Venoxis",
+      zh_TW = "溫諾希斯",
+      ko_KR = "Venoxis",
+      en_US = "Venoxis",
+      es_MX = "Venoxis",
+      pt_BR = "Venoxis",
+      es_ES = "Venoxis",
+      zh_CN = "温诺希斯",
+      fr_FR = "Venoxis",
+      de_DE = "Venoxis"
+    }
+  },
+  {
+    id = 4464,
+    slug = "sulfuron",
+    name = {
+      it_IT = "Sulfuron",
+      ru_RU = "Sulfuron",
+      en_GB = "Sulfuron",
+      zh_TW = "薩弗隆",
+      ko_KR = "Sulfuron",
+      en_US = "Sulfuron",
+      es_MX = "Sulfuron",
+      pt_BR = "Sulfuron",
+      es_ES = "Sulfuron",
+      zh_CN = "萨弗隆",
+      fr_FR = "Sulfuron",
+      de_DE = "Sulfuron"
+    }
+  },
+  {
+    id = 4441,
+    slug = "auberdine",
+    name = {
+      it_IT = "Auberdine",
+      ru_RU = "Auberdine",
+      en_GB = "Auberdine",
+      zh_TW = "奧伯丁",
+      ko_KR = "Auberdine",
+      en_US = "Auberdine",
+      es_MX = "Auberdine",
+      pt_BR = "Auberdine",
+      es_ES = "Auberdine",
+      zh_CN = "奥伯丁",
+      fr_FR = "Auberdine",
+      de_DE = "Auberdine"
+    }
+  },
+  {
+    id = 4745,
+    slug = "transcendence",
+    name = {
+      it_IT = "Transcendence",
+      ru_RU = "Превосходство",
+      en_GB = "Transcendence",
+      zh_TW = "超凡入聖",
+      ko_KR = "Transcendence",
+      en_US = "Transcendence",
+      es_MX = "Transcendence",
+      pt_BR = "Transcendence",
+      es_ES = "Transcendence",
+      zh_CN = "卓越",
+      fr_FR = "Transcendence",
+      de_DE = "Transcendence"
+    }
+  },
+  {
+    id = 4742,
+    slug = "ashbringer",
+    name = {
+      it_IT = "Ashbringer",
+      ru_RU = "Испепелитель",
+      en_GB = "Ashbringer",
+      zh_TW = "灰燼使者",
+      ko_KR = "Ashbringer",
+      en_US = "Ashbringer",
+      es_MX = "Ashbringer",
+      pt_BR = "Ashbringer",
+      es_ES = "Ashbringer",
+      zh_CN = "灰烬使者",
+      fr_FR = "Ashbringer",
+      de_DE = "Ashbringer"
+    }
+  },
+  {
+    id = 4816,
+    slug = "jindo",
+    name = {
+      it_IT = "Jin'do",
+      ru_RU = "Jin'do",
+      en_GB = "Jin'do",
+      zh_TW = "金度",
+      ko_KR = "Jin'do",
+      en_US = "Jin'do",
+      es_MX = "Jin'do",
+      pt_BR = "Jin'do",
+      es_ES = "Jin'do",
+      zh_CN = "金度",
+      fr_FR = "Jin'do",
+      de_DE = "Jin'do"
+    }
+  },
+  {
+    id = 4453,
+    slug = "pyrewood-village",
+    name = {
+      it_IT = "Pyrewood Village",
+      ru_RU = "Pyrewood Village",
+      en_GB = "Pyrewood Village",
+      zh_TW = "焚木村",
+      ko_KR = "Pyrewood Village",
+      en_US = "Pyrewood Village",
+      es_MX = "Pyrewood Village",
+      pt_BR = "Pyrewood Village",
+      es_ES = "Pyrewood Village",
+      zh_CN = "焚木村",
+      fr_FR = "Pyrewood Village",
+      de_DE = "Pyrewood Village"
+    }
+  },
+  {
+    id = 4467,
+    slug = "firemaw",
+    name = {
+      it_IT = "Firemaw",
+      ru_RU = "Firemaw",
+      en_GB = "Firemaw",
+      zh_TW = "費爾默",
+      ko_KR = "Firemaw",
+      en_US = "Firemaw",
+      es_MX = "Firemaw",
+      pt_BR = "Firemaw",
+      es_ES = "Firemaw",
+      zh_CN = "费尔默",
+      fr_FR = "Firemaw",
+      de_DE = "Firemaw"
+    }
+  },
+  {
+    id = 4465,
+    slug = "golemagg",
+    name = {
+      it_IT = "Golemagg",
+      ru_RU = "Golemagg",
+      en_GB = "Golemagg",
+      zh_TW = "古雷曼格",
+      ko_KR = "Golemagg",
+      en_US = "Golemagg",
+      es_MX = "Golemagg",
+      pt_BR = "Golemagg",
+      es_ES = "Golemagg",
+      zh_CN = "古雷曼格",
+      fr_FR = "Golemagg",
+      de_DE = "Golemagg"
+    }
+  },
+  {
+    id = 4442,
+    slug = "lakeshire",
+    name = {
+      it_IT = "Lakeshire",
+      ru_RU = "Приозерье",
+      en_GB = "Lakeshire",
+      zh_TW = "湖畔鎮",
+      ko_KR = "Lakeshire",
+      en_US = "Lakeshire",
+      es_MX = "Lakeshire",
+      pt_BR = "Lakeshire",
+      es_ES = "Lakeshire",
+      zh_CN = "湖畔镇",
+      fr_FR = "Lakeshire",
+      de_DE = "Lakeshire"
+    }
+  },
+  {
+    id = 4701,
+    slug = "mograine",
+    name = {
+      it_IT = "Mograine",
+      ru_RU = "Mograine",
+      en_GB = "Mograine",
+      zh_TW = "莫格萊尼",
+      ko_KR = "Mograine",
+      en_US = "Mograine",
+      es_MX = "Mograine",
+      pt_BR = "Mograine",
+      es_ES = "Mograine",
+      zh_CN = "莫格莱尼",
+      fr_FR = "Mograine",
+      de_DE = "Mograine"
+    }
+  },
+  {
+    id = 4474,
+    slug = "flamegor",
+    name = {
+      it_IT = "Flamegor",
+      ru_RU = "Пламегор",
+      en_GB = "Flamegor",
+      zh_TW = "弗萊格爾",
+      ko_KR = "Flamegor",
+      en_US = "Flamegor",
+      es_MX = "Flamegor",
+      pt_BR = "Flamegor",
+      es_ES = "Flamegor",
+      zh_CN = "弗莱格尔",
+      fr_FR = "Flamegor",
+      de_DE = "Пламегор"
+    }
+  },
+  {
+    id = 4476,
+    slug = "gehennas",
+    name = {
+      it_IT = "Gehennas",
+      ru_RU = "Gehennas",
+      en_GB = "Gehennas",
+      zh_TW = "基赫納斯",
+      ko_KR = "Gehennas",
+      en_US = "Gehennas",
+      es_MX = "Gehennas",
+      pt_BR = "Gehennas",
+      es_ES = "Gehennas",
+      zh_CN = "基赫纳斯",
+      fr_FR = "Gehennas",
+      de_DE = "Gehennas"
+    }
+  },
+  {
+    id = 4815,
+    slug = "thekal",
+    name = {
+      it_IT = "Thekal",
+      ru_RU = "Thekal",
+      en_GB = "Thekal",
+      zh_TW = "塞卡爾",
+      ko_KR = "Thekal",
+      en_US = "Thekal",
+      es_MX = "Thekal",
+      pt_BR = "Thekal",
+      es_ES = "Thekal",
+      zh_CN = "塞卡尔",
+      fr_FR = "Thekal",
+      de_DE = "Thekal"
+    }
+  },
+  {
+    id = 4456,
+    slug = "nethergarde-keep",
+    name = {
+      it_IT = "Nethergarde Keep",
+      ru_RU = "Nethergarde Keep",
+      en_GB = "Nethergarde Keep",
+      zh_TW = "守望堡",
+      ko_KR = "Nethergarde Keep",
+      en_US = "Nethergarde Keep",
+      es_MX = "Nethergarde Keep",
+      pt_BR = "Nethergarde Keep",
+      es_ES = "Nethergarde Keep",
+      zh_CN = "守望堡",
+      fr_FR = "Nethergarde Keep",
+      de_DE = "Nethergarde Keep"
+    }
+  },
+  {
+    id = 4454,
+    slug = "mirage-raceway",
+    name = {
+      it_IT = "Mirage Raceway",
+      ru_RU = "Mirage Raceway",
+      en_GB = "Mirage Raceway",
+      zh_TW = "沙漠賽道",
+      ko_KR = "Mirage Raceway",
+      en_US = "Mirage Raceway",
+      es_MX = "Mirage Raceway",
+      pt_BR = "Mirage Raceway",
+      es_ES = "Mirage Raceway",
+      zh_CN = "沙漠赛道",
+      fr_FR = "Mirage Raceway",
+      de_DE = "Mirage Raceway"
+    }
+  },
+  {
+    id = 4452,
+    slug = "chromie",
+    name = {
+      it_IT = "Chromie",
+      ru_RU = "Хроми",
+      en_GB = "Chromie",
+      zh_TW = "克羅米",
+      ko_KR = "Chromie",
+      en_US = "Chromie",
+      es_MX = "Chromie",
+      pt_BR = "Chromie",
+      es_ES = "Chromie",
+      zh_CN = "克罗米",
+      fr_FR = "Chromie",
+      de_DE = "Хроми"
+    }
+  },
+  {
+    id = 4466,
+    slug = "patchwerk",
+    name = {
+      it_IT = "Patchwerk",
+      ru_RU = "Patchwerk",
+      en_GB = "Patchwerk",
+      zh_TW = "縫補者",
+      ko_KR = "Patchwerk",
+      en_US = "Patchwerk",
+      es_MX = "Patchwerk",
+      pt_BR = "Patchwerk",
+      es_ES = "Patchwerk",
+      zh_CN = "帕奇维克",
+      fr_FR = "Patchwerk",
+      de_DE = "Patchwerk"
+    }
+  },
+  {
+    id = 4703,
+    slug = "amnennar",
+    name = {
+      it_IT = "Amnennar",
+      ru_RU = "Amnennar",
+      en_GB = "Amnennar",
+      zh_TW = "亞門納爾",
+      ko_KR = "Amnennar",
+      en_US = "Amnennar",
+      es_MX = "Amnennar",
+      pt_BR = "Amnennar",
+      es_ES = "Amnennar",
+      zh_CN = "亚门纳尔",
+      fr_FR = "Amnennar",
+      de_DE = "Amnennar"
+    }
+  },
+  {
+    id = 4440,
+    slug = "everlook",
+    name = {
+      it_IT = "Everlook",
+      ru_RU = "Everlook",
+      en_GB = "Everlook",
+      zh_TW = "永望鎮",
+      ko_KR = "Everlook",
+      en_US = "Everlook",
+      es_MX = "Everlook",
+      pt_BR = "Everlook",
+      es_ES = "Everlook",
+      zh_CN = "永望镇",
+      fr_FR = "Everlook",
+      de_DE = "Everlook"
+    }
+  },
+  {
+    id = 4811,
+    slug = "giantstalker",
+    name = {
+      it_IT = "Giantstalker",
+      ru_RU = "Giantstalker",
+      en_GB = "Giantstalker",
+      zh_TW = "巨人巡者",
+      ko_KR = "Giantstalker",
+      en_US = "Giantstalker",
+      es_MX = "Giantstalker",
+      pt_BR = "Giantstalker",
+      es_ES = "Giantstalker",
+      zh_CN = "巨人追猎者",
+      fr_FR = "Giantstalker",
+      de_DE = "Giantstalker"
+    }
+  },
+  {
+    id = 4813,
+    slug = "mandokir",
+    name = {
+      it_IT = "Mandokir",
+      ru_RU = "Mandokir",
+      en_GB = "Mandokir",
+      zh_TW = "曼多基爾",
+      ko_KR = "Mandokir",
+      en_US = "Mandokir",
+      es_MX = "Mandokir",
+      pt_BR = "Mandokir",
+      es_ES = "Mandokir",
+      zh_CN = "曼多基尔",
+      fr_FR = "Mandokir",
+      de_DE = "Mandokir"
+    }
+  }
+}
+
+-- Export the table
+return euRealms
